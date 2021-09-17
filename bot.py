fe9818d5a36973cda7b16dc504fa2fef1074963c
davilbs <davilage.borges@gmail.com>
2021-09-17 10:39:56 -0300

    Added queue and skip

--- bot.py
+++ bot.py
@@ -8,33 +8,34 @@ cogs = [music]
 
 client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 
-@client.event
-async def on_ready():
-    print("Bot iniciado!")
+# @client.event
+# async def on_ready():
+#     print("Bot iniciado!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    with open('nouns.txt', 'r') as f:
-        nouns = f.read().splitlines()
-    with open('adjectives.txt', 'r') as f:
-        adjectives = f.read().splitlines()
+#     with open('nouns.txt', 'r') as f:
+#         nouns = f.read().splitlines()
+#     with open('adjectives.txt', 'r') as f:
+#         adjectives = f.read().splitlines()
 
-    noun = nouns[random.randint(0, len(nouns))]
-    adjective = adjectives[random.randint(0, len(adjectives))]
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    if message.content == 'aleatorio' or message.content == 'aleatório':
-        rand_name = "You are a"
-        if adjective[0] in vowels:
-            rand_name += 'n'
+#     noun = nouns[random.randint(0, len(nouns))]
+#     adjective = adjectives[random.randint(0, len(adjectives))]
+#     vowels = ['a', 'e', 'i', 'o', 'u']
+#     if message.content == 'aleatorio' or message.content == 'aleatório':
+#         rand_name = "You are a"
+#         if adjective[0] in vowels:
+#             rand_name += 'n'
 
-        name = " " + adjective + " " + noun
-        rand_name += name
+#         name = " " + adjective + " " + noun
+#         rand_name += name
         
-        await message.channel.send(rand_name)
+#         await message.channel.send(rand_name)
 
+print("Inicializando cogs...")
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
--- music.py
+++ music.py
@@ -1,62 +1,116 @@
 import discord
+import asyncio
 from discord.ext import commands
+from discord.ext.commands.core import command
 import youtube_dl
 
 class music(commands.Cog):
+    songs_queue: list
+    my_ctx: commands.Context
+
     def __init__(self, client):
         self.client = client
+        self.songs_queue = []
 
     @commands.command()
-    async def join(self, ctx):
+    async def join(self, ctx: commands.Context):
         if ctx.author.voice is None:
             await ctx.send("Você não tá no voice")
             return False
         voice_channel = ctx.author.voice.channel
-        print("joining channel")
+        # TODO check if bot is playing already
         if ctx.voice_client is None:
+            print("joining channel")
             await voice_channel.connect()
-        else:
-            await ctx.voice_client.move_to(voice_channel)
+        elif ctx.voice_client.channel != voice_channel:
+            if not ctx.voice_client.is_playing():
+                print("changin g channel")
+                await ctx.voice_client.move_to(voice_channel)
         return True
         
     
-    @commands.command()
-    async def leave(self, ctx):
+    @commands.command(aliases=['l', 'leave'])
+    async def disconnect(self, ctx: commands.Context):
+        await ctx.send("So long, farewell, no more sailing...")
         await ctx.voice_client.disconnect()
 
-    @commands.command()
-    async def p(self, ctx, url = ''):
+    @commands.command(aliases=['p'])
+    async def play(self, ctx: commands.Context, url: str = ''):
         if url == '':
-            await ctx.send("url nao especificada")
+            await ctx.send("URL não especificada")
             return
-        print("trying to join")
+        # print(f"trying to join {ctx.author.channel.name}")
         if not await self.join(ctx):
             return
-        ctx.voice_client.stop()
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
-        'options': '-vn'}
+        info = self.get_song_info(url)
+        await self.add_song_to_queue(ctx,info)
+        if len(self.songs_queue) == 1:
+            await self.play_song(ctx, info)
+
+    async def add_song_to_queue(self, ctx: commands.Context, info: dict,):
+        self.songs_queue.append(info)
+        await ctx.send(f"Song {info['title']} added to queue! Queue has {len(self.songs_queue)} songs...")
+
+    def get_song_info(self, url: str):
         YDL_OPTIONS = {'format': 'bestaudio'}
-        vc = ctx.voice_client
 
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-            vc.play(source)
+            return ydl.extract_info(url, download=False)
+
+    async def play_song(self, ctx: commands.Context, info: dict):
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
+        'options': '-vn'}
+        print(f"Now Playing song {info['title']}")
+        vc = ctx.voice_client
+        yt_url = info['formats'][0]['url']
+        source = await discord.FFmpegOpusAudio.from_probe(yt_url, **FFMPEG_OPTIONS)
+        self.my_ctx = ctx
+        vc.play(source, after=self.after_play)
 
     @commands.command()
-    async def pause(self, ctx):
-        await ctx.voice_client.pause()
+    async def pause(self, ctx: commands.Context):
+        ctx.voice_client.pause()
         await ctx.send("parey parey")
 
     @commands.command()
-    async def resume(self, ctx):
-        await ctx.voice_client.resume()
+    async def resume(self, ctx: commands.Context):
+        ctx.voice_client.resume()
         await ctx.send("voltey voltey")
 
+    @commands.command(aliases=['q'])
+    async def queue(self, ctx: commands.Context):
+        if len(self.songs_queue) == 0:
+            await ctx.send("The current queue is empty :(")  
+            return  
+        queue_list = ''
+        for song in self.songs_queue:
+            queue_list += song['title']
+            queue_list += "\n"
+        await ctx.send(queue_list)
+
     @commands.command()
-    async def queue(self, ctx):
-        await ctx.send("Calma que ja vai ta pronto daqui a pouco")
+    async def skip(self, ctx: commands.Context):
+        vc = ctx.voice_client
+        if vc is None:
+            await ctx.send("Not in a voice channel")
+            return
+        self.songs_queue = self.songs_queue[1:]
+        vc.stop()
+        if len(self.songs_queue) > 0:
+            self.play_song(ctx, self.songs_queue[0])
+
+    def after_play(self, error):
+        self.songs_queue = self.songs_queue[1:]
+        if len(self.songs_queue) > 0:
+            coro = self.play_song(self.my_ctx, self.songs_queue[0])
+        else:
+            coro = self.disconnect(self.my_ctx)
+        fut = asyncio.run_coroutine_threadsafe(coro, self.client.loop)
+        try:
+            fut.result()
+        except:
+            pass
+
 
 def setup(client):
     client.add_cog(music(client))
